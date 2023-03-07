ff40c884444fbfb96c0e5e4e114bc83f679a528f
GithubUserXyz <github_user_xyz@aol.com>
2023-03-07 17:32:39 +0900

    edit todo.py

--- prog/todo.py
+++ prog/todo.py
@@ -3,9 +3,25 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def todo():
+def root():
     return "<h1>Todo</h1>"
 
 @app.route("/about")
 def about():
     return "<h1>About</h1>"
+
+@app.get("/todo")
+def todo_get():
+    return "<h1>get item</h1>"
+
+@app.post("/todo")
+def todo_post():
+    return "<h1>post item</h1>"
+
+@app.put("/todo")
+def todo_put():
+    return "<h1>put item</h1>"
+
+@app.delete("/todo")
+def todo_delete():
+    return "<h1>delete item</h1>"
