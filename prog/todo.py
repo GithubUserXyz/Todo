ed6ba7496165aed9b71e3661a0f2f4f602d24245
GithubUserXyz <github_user_xyz@aol.com>
2023-03-11 21:02:24 +0900

    edit todo.py

--- prog/todo.py
+++ prog/todo.py
@@ -1,11 +1,15 @@
-from flask import Flask, jsonify
+import logging
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False
+# data base
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todo.db"
 db.init_app(app)
+# logging
+logging.basicConfig(level=logging.DEBUG)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -20,23 +24,48 @@ def root():
 def about():
     return "<h1>About</h1>"
 
-@app.get("/todo")
+###############################################################
+# todoの一覧を返す
+#
+@app.get("/api/todos")
 def todo_get():
+    todos = db.session.execute(db.select(Todo).order_by(Todo.id)).scalars()
+    app.logger.debug(todos)
+    todos_dict = \
+        [
+            {'id': t.id, 'title': t.title, 'description': t.description}  for t in todos
+        ]
+    app.logger.debug(todos_dict)
     # 200 OKステータスコードと一書に返答
-    return jsonify({"title":"todo title"}), 200
+    return jsonify(todos_dict), 200
 
-@app.post("/todo")
+
+###############################################################
+# todoを追加
+#
+@app.post("/api/todos")
 def todo_post():
+    # Valication
+    app.logger.debug("title "+request.json["title"])
+    app.logger.debug("desciption "+request.json["description"])
+    # data create
+    todo = Todo(
+        title=request.json["title"],
+        description=request.json["description"],
+    )
+    db.session.add(todo)
+    db.session.commit()
+    # return
     return jsonify({
-        "id":1,
-        "title":"todo title",
-        "description":"todo description"
+        "id":todo.id,
+        "title":todo.title,
+        "description":todo.description
         }), 200
 
-@app.put("/todo")
+@app.put("/api/todos")
 def todo_put():
     return "<h1>put item</h1>"
 
-@app.delete("/todo")
+@app.delete("/api/todos")
 def todo_delete():
     return "<h1>delete item</h1>"
