57541424697f83c0b0741824e7008f2a82e33b74
GithubUserXyz <github_user_xyz@aol.com>
2023-03-15 13:35:22 +0900

    add main.dart

---
+++ orange/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo',
+      theme: ThemeData(primaryColor: Colors.blue),
+      debugShowCheckedModeBanner: true,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Todo List'),
+          actions: <Widget>[
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.arrow_back),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.arrow_upward),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.arrow_downward),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.arrow_forward),
+            ),
+            IconButton(
+              onPressed: () {}, 
+              icon: const Icon(Icons.favorite),
+            ),
+            IconButton(
+              onPressed: () {}, 
+              icon: const Icon(Icons.more_vert),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
