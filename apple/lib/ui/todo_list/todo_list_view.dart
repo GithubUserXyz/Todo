fec6ddf16f3b692f68fb23521480a7038c34da00
GithubUserXyz <github_user_xyz@aol.com>
2023-04-08 14:13:07 +0900

    list view とdetail viewをくっつけた

--- apple/lib/ui/todo_list/todo_list_view.dart
+++ apple/lib/ui/todo_list/todo_list_view.dart
@@ -1,4 +1,7 @@
+import 'dart:developer';
+
 import 'package:apple/model/repository/todo_repository_api.dart';
+import 'package:apple/ui/todo_detail/todo_detail_view.dart';
 import 'package:apple/ui/todo_list/todo_list_view_model.dart';
 import 'package:apple/model/entity/todo.dart';
 import 'package:flutter/material.dart';
@@ -40,15 +43,35 @@ class _TodoListPage extends StatelessWidget {
     return ListView.builder(
       itemCount: vm.todos.length,
       itemBuilder: (context, index) {
-        var todo = vm.todos[index];
-        return Text(todo.title);
+        return _buildTodoCard(context, vm.todos[index]);
       },
     );
   }
 
   Widget _buildTodoCard(BuildContext context, Todo todo) {
-    return Card(
-      child: Text(todo.title),
+    return GestureDetector(
+      onTap: () {
+        log("'${todo.title}' tapped.");
+        _navigateAndDisplayTodoDetailPage(context, todo);
+      },
+      child: Card(
+        child: Column(
+          children: <Widget>[
+            ListTile(
+              title: Text(todo.title),
+              subtitle: Text(todo.description),
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+  void _navigateAndDisplayTodoDetailPage(BuildContext context, Todo todo) {
+    var route = MaterialPageRoute(
+      settings: const RouteSettings(name: '/todo_detail'),
+      builder: (BuildContext context) => TodoDetailView(todo: todo),
+    );
+    Navigator.push(context, route);
   }
 }
