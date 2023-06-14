50d5f99a49d1a4ac74d6e5c2dd6834e9296feb1f
GithubUserXyz <github_user_xyz@aol.com>
2023-06-14 14:28:00 +0900

    add api

---
+++ grape/lib/model/api/todo_api.dart
@@ -0,0 +1,14 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+import '../entity/todo.dart';
+import '../repository/todo_repository.dart';
+
+class TodoApi implements TodoRepository {
+  @override
+  Future<List<Todo>> readAllTodos() async {
+    var response = await http.get(Uri.http('127.0.0.1:5000', 'api/todos'));
+    var jsonRes = jsonDecode(utf8.decode(response.bodyBytes));
+    return jsonRes.map((map) => Todo.fromJson(map)).toList();
+  }
+}
--- grape/lib/model/repository/todo_repository.dart
+++ grape/lib/model/repository/todo_repository.dart
@@ -1,1 +1,5 @@
+import '../entity/todo.dart';
 
+abstract class TodoRepository {
+  Future<List<Todo>> readAllTodos();
+}
