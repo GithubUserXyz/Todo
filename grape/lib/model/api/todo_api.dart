5e6dcdd3488019adfc259f0fba6fff35a13711ff
GithubUserXyz <github_user_xyz@aol.com>
2023-06-15 17:58:01 +0900

    change todo_api.dart

--- grape/lib/model/api/todo_api.dart
+++ grape/lib/model/api/todo_api.dart
@@ -14,4 +14,65 @@ class TodoApi implements TodoRepository {
 
     return jsonRes.map<Todo>((map) => Todo.fromJson(map)).toList();
   }
+
+  @override
+  Future<Todo?> readTodoById(int id) async {
+    var response = await http.get(Uri.http('127.0.0.1:5000', 'api/todos/$id'));
+    if (response.statusCode == 200) {
+      var jsonRes = jsonDecode(utf8.decode(response.bodyBytes));
+      return Todo.fromJson(jsonRes);
+    } else {
+      return null;
+    }
+  }
+
+  @override
+  Future<bool> createTodo(String title, String description) async {
+    Map<String, String> headers = {'content-type': 'application/json'};
+    var body = jsonEncode({
+      'title': title,
+      'description': description,
+    });
+    final response = await http.post(
+      Uri.http('127.0.0.1:5000', 'api/todos'),
+      headers: headers,
+      body: body,
+    );
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  @override
+  Future<bool> updateTodo(Todo todo) async {
+    Map<String, String> headers = {'content-type': 'application/json'};
+    var body = jsonEncode({
+      'title': todo.title,
+      'description': todo.description,
+    });
+    final response = await http.put(
+      Uri.http('127.0.0.1:5000', 'api/todos/${todo.id}'),
+      headers: headers,
+      body: body,
+    );
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  @override
+  Future<bool> deleteTodoById(int id) async {
+    final response =
+        await http.delete(Uri.http('127.0.0.1:5000', 'api/todos/$id'));
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      return false;
+      //throw Exception('Failed to delete Todo');
+    }
+  }
 }
--- grape/test/model/todo_repository_test.dart
+++ grape/test/model/todo_repository_test.dart
@@ -1,9 +1,50 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:grape/model/api/todo_api.dart';
+import 'package:grape/model/entity/todo.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   test('repository api todo', () async {
     var api = TodoApi();
-    api.readAllTodos();
+    await api.readAllTodos();
+  });
+
+  test('create todo', () async {
+    var api = TodoApi();
+    await api.createTodo('test1 title', 'test1 description');
+  });
+
+  test('read todo', () async {
+    var api = TodoApi();
+    Todo? todo = await api.readTodoById(6);
+    expect(todo, isNotNull);
+    expect(todo, isInstanceOf<Todo>());
+  });
+
+  test('update todo', () async {
+    var api = TodoApi();
+    Todo? todo = await api.readTodoById(6);
+
+    expect(todo, isNotNull);
+    expect(todo, isInstanceOf<Todo>());
+
+    var now = DateTime.now();
+    String nowStr = DateFormat('yyyyMMddhhmm').format(now);
+    String updateTitle = todo!.title + nowStr;
+    todo.title = updateTitle;
+    await api.updateTodo(todo);
+
+    Todo? todo2 = await api.readTodoById(6);
+    expect(todo2, isNotNull);
+    expect(todo2, isInstanceOf<Todo>());
+    expect(todo2!.title, equals(updateTitle));
+  });
+
+  test('delete todo', () async {
+    var api = TodoApi();
+    await api.deleteTodoById(6);
+
+    Todo? todo = await api.readTodoById(6);
+    expect(todo, isNull);
   });
 }
