4b295c9d43c96984a9b466702960d9c4cda2ed8f
GithubUserXyz <github_user_xyz@aol.com>
2023-06-29 12:27:13 +0900

    edit apple

--- apple/test/integration_test/test.dart
+++ apple/test/integration_test/test.dart
@@ -1,8 +1,5 @@
-import 'package:flutter_driver/driver_extension.dart';
-import 'package:integration_test/integration_test.dart';
-import 'package:integration_test/integration_test_driver.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter/material.dart';
+import 'package:integration_test/integration_test.dart';
 
 import 'package:apple/main.dart' as app;
 
